ab409093f32082b6e21547c6247de68a37911b8e
naelin-cmd <56066212+naelin-cmd@users.noreply.github.com>
2019-11-22 12:17:45 +0200

    ListViewExercise

---
+++ src/exercise1/start/FlagFacts/AllFlagsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagFacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlagsPage : ContentPage
+    {
+        public AllFlagsPage()
+        {
+
+            InitializeComponent();
+
+            BindingContext = DependencyService.Get<FlagDetailsViewModel>();
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            await this.Navigation.PushAsync(new FlagDetailsPage());
+        }
+    }
+}
